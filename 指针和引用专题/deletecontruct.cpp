a70920b0d725efc24e21ddbd78ce766762dc474a
navigator <524631266@qq.com>
2020-08-18 11:37:11 +0800

    添加一个正常的类的创建

---
+++ 指针和引用专题/deletecontruct.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Abc{
+private:
+    int a;
+public:
+    // 删除copy构造函数
+    // Abc(const Abc&) = delete;
+    // Abc& operator=(const Abc&) = delete;
+    Abc(){
+        a = 1;
+    };
+    Abc(const int a):a(a){};
+    void print(){
+        cout << "a is " << a << endl;
+    }
+
+    ~Abc(){
+        cout << "destroy an object Abc" << endl;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Abc ab = Abc();
+    ab.print();
+    return 0;
+}
